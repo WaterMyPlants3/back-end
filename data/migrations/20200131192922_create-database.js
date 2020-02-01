4f45b72e6b8f4c3c8608630ddf30e554b7fc34f9
Reece Pierson <piersonreece@gmail.com>
2020-01-31 20:22:33 -0600

    initialized, added dependencies, built DB

---
+++ data/migrations/20200131192922_create-database.js
@@ -0,0 +1,45 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('users', tbl => {
+      tbl.increments();
+      tbl.string('username')
+        .unique()
+        .notNullable();
+      tbl.string('phoneNumber')
+        .unique();
+      tbl.string('password')
+        .notNullable();
+  })
+  .createTable('plants', tbl=> {
+      tbl.increments();
+      tbl.string('species')
+        .unique()
+        .notNullable()
+  })
+  .createTable('users_plants', tbl =>{
+      tbl.integer('userKey')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('users')
+        .onUpdate('CASCADE')
+        .onDelete('RESTRICT');
+      tbl.integer('plantKey')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('plants')
+        .onUpdate('CASCADE')
+        .onDelete('RESTRICT');
+      tbl.string('nickName');
+      tbl.int('h2oFrequency')
+        .notNullable();
+      tbl.specificType('image', 'image');
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('users_plants')
+    .dropTableIfExists('plants')
+    .dropTableIfExists('users')
+};
